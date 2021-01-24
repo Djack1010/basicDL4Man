107a068b9f2b49b91ea9fd1dde38f78723569b7c
djack1010 <giachi.iada@gmail.com>
2021-01-24 12:29:09 +0100

    improve main, add mode parameter and better handler dataset and models

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import re
 import numpy as np
 import pathlib
 import tensorflow as tf
-from models.basic import BASIC
+from models.basic_cnn import BasicCNN
+from models.basic_mlp import BasicMLP
 from models.VGG16 import VGG16_19
 from utils.config import timeExec, MAIN_PATH, time, os
 
 
 def main(arguments):
-
-    print_log("STARTING EXECUTION AT\t{}".format(time.strftime("%d-%m %H:%M:%S")), print_on_screen=True)
     print("LOADING AND PRE-PROCESSING DATA")
 
     dataset_base = MAIN_PATH + arguments.dataset
@@ -28,61 +27,90 @@ def main(arguments):
     size_test = sum([len(files) for r, d, files in os.walk(dataset_base + "/test")])
     nclasses = len(CLASS_NAMES)
 
+    # Print information on log
+    # EXECUTION Info
+    print_log(f"INFO EXECUTION:"
+              f"\nmode = {arguments.mode}\nmodel = {arguments.model}\ndataset = {arguments.dataset}"
+              f"\noutput_model = {arguments.output_model}\nepochs = {arguments.epochs}\nbatch_size = {arguments.batch_size}"
+              f"\n----------------")
+
+    # DATA Info
+    print_log(f"INFO DATA:"
+              f"\nnum_classes = {nclasses}\nclass_names= {CLASS_NAMES}"
+              f"\nSize train-val-test= {size_train}-{size_val}-{size_test}"
+              f"\nsize_img = {arguments.image_size}x{arguments.channels}")
+
+    print_log("STARTING EXECUTION AT\t{}".format(time.strftime("%d-%m %H:%M:%S")), print_on_screen=True)
+
     # SELECTING MODELS
     model = _model_selection(arguments, nclasses)
 
     # --------------  TRAINING and VALIDATION part  --------------------
 
-    #  Use Dataset.map to create a dataset of image, label pairs
-    # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
-    lab_train_ds = train_paths_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-    lab_val_ds = val_paths_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+    if arguments.mode == "train-val":
+        #  Use Dataset.map to create a dataset of image, label pairs
+        # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+        lab_train_ds = train_paths_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+        lab_val_ds = val_paths_ds.map(process_path, num_parallel_calls=AUTOTUNE)
 
-    train_ds = prepare_for_training(lab_train_ds)
-    val_ds = prepare_for_training(lab_val_ds)
+        train_ds = prepare_for_training(lab_train_ds)
+        val_ds = prepare_for_training(lab_val_ds)
 
-    print_log('Start Training for {} epochs  '.format(arguments.epochs), print_on_screen=True)
+        print_log('Start Training for {} epochs  '.format(arguments.epochs), print_on_screen=True)
 
-    # Initialize callbacks for Tensorboard
-    log_fit = "tensorboard/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback_fit = tf.keras.callbacks.TensorBoard(log_dir=log_fit, histogram_freq=1)
+        # Initialize callbacks for Tensorboard
+        log_fit = "tensorboard/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        tensorboard_callback_fit = tf.keras.callbacks.TensorBoard(log_dir=log_fit, histogram_freq=1)
 
-    train_results = model.fit(x=train_ds, batch_size=arguments.batch_size, epochs=arguments.epochs,
-                              validation_data=val_ds, callbacks=[tensorboard_callback_fit])
+        train_results = model.fit(x=train_ds, batch_size=arguments.batch_size, epochs=arguments.epochs,
+                                  validation_data=val_ds, callbacks=[tensorboard_callback_fit])
 
-    print_log("\ttraining loss: {} \n\ttraining acc:{} \n\tvalidation loss:{} \n\tvalidation acc:{}"
-              .format(train_results.history['loss'], train_results.history['acc'],
-                      train_results.history['val_loss'], train_results.history['val_acc']))
+        print_log("\ttrain_loss:{} \n\ttrain_acc:{} \n\ttrain_prec:{} \n\ttrain_rec:{} \n"
+                  "\tval_loss:{} \n\tval_acc:{} \n\tval_prec:{} \n\tval_rec:{}"
+                  .format(train_results.history['loss'], train_results.history['prec'], train_results.history['rec'],
+                          train_results.history['acc'],
+                          train_results.history['val_loss'], train_results.history['val_acc'],
+                          train_results.history['val_prec'], train_results.history['val_rec']))
 
-    del train_ds, val_ds
+        del train_ds, val_ds
 
     # --------------  FINAL TRAINING and TEST part  --------------------
 
-    #  Use Dataset.map to create a dataset of image, label pairs
-    # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
-    lab_final_train_ds = final_training_paths_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-    lab_test_ds = test_paths_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+    if arguments.mode == "train-test":
+        #  Use Dataset.map to create a dataset of image, label pairs
+        # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+        lab_final_train_ds = final_training_paths_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+        lab_test_ds = test_paths_ds.map(process_path, num_parallel_calls=AUTOTUNE)
 
-    fin_train_ds = prepare_for_training(lab_final_train_ds)
-    test_ds = prepare_for_training(lab_test_ds)
+        fin_train_ds = prepare_for_training(lab_final_train_ds)
+        test_ds = prepare_for_training(lab_test_ds)
 
-    # Train the model over the entire total_training set and then test
-    print_log('Start Final Training for {} epochs  '.format(arguments.epochs), print_on_screen=True)
-    final_train_results = model.fit(x=fin_train_ds, batch_size=arguments.batch_size, epochs=arguments.epochs)
-    print_log("\ttraining loss: {} \n\ttraining acc:{}" .format(final_train_results.history['loss'],
-                                                            final_train_results.history['acc']))
+        # Train the model over the entire total_training set and then test
+        print_log('Start Final Training for {} epochs  '.format(arguments.epochs), print_on_screen=True)
+        final_train_results = model.fit(x=fin_train_ds, batch_size=arguments.batch_size, epochs=arguments.epochs)
+        print_log("\ttrain_loss:{} \n\ttrain_acc:{} \n\ttrain_prec:{} \n\ttrain_rec:{} \n"
+                  .format(final_train_results.history['loss'], final_train_results.history['prec'],
+                          final_train_results.history['rec'], final_train_results.history['acc']))
 
-    # Test the trained model over the test set
-    print_log('Start Test', print_on_screen=True)
-    results = model.evaluate(test_ds)
-    print_log("\ttest loss: {} \n\ttest accuracy: {}".format(results[0], results[1]))
+        # Test the trained model over the test set
+        print_log('Start Test', print_on_screen=True)
+        results = model.evaluate(test_ds)
+        print_log("\ttest loss:{} \n\ttest accuracy:{}".format(results[0], results[1]), print_on_screen=True)
+        print_log("\tPrec:{} \n\tRecall:{}".format(results[2], results[3]), print_on_screen=True)
+        try:
+            # F-measure calculated as (2 * Prec * Recall)/(Prec + Recall)
+            print_log("\tF-Measure:{} \n\tAUC:{}"
+                      .format((2 * results[2] * results[3]) / (results[2] + results[3]), results[4]), print_on_screen=True)
+        except ZeroDivisionError:
+            print_log("\tF-Measure:{} \n\tAUC:{}"
+                      .format("Error", results[4]), print_on_screen=True)
 
-    del fin_train_ds, test_ds
+        del fin_train_ds, test_ds
 
-    # save model and architecture to single file
-    if arguments.output_model is not None:
-        model.save(MAIN_PATH + 'model_saved/{}_m{}'.format(arguments.output_model, arguments.model))
-        model.save_weights(MAIN_PATH + 'model_saved/{}_m{}_weights'.format(arguments.output_model, arguments.model))
+        # save model and architecture to single file
+        if arguments.output_model is not None:
+            model.save(MAIN_PATH + 'model_saved/{}_m{}'.format(arguments.output_model, arguments.model))
+            model.save_weights(MAIN_PATH + 'model_saved/{}_m{}_weights'.format(arguments.output_model, arguments.model))
 
     print_log("ENDING EXECUTION AT\t{}".format(time.strftime("%d-%m %H:%M:%S")), print_on_screen=True)
 
@@ -92,13 +120,14 @@ def parse_args():
         description='Deep Learning Image Malware Classification')
     group = parser.add_argument_group('Arguments')
     # REQUIRED Arguments
-    group.add_argument('-m', '--model', required=True, type=str,
-                       help='BASIC or VGG16')
+    group.add_argument('-m', '--model', required=True, type=str, choices=['BASIC_CNN', 'BASIC_MLP', 'VGG16'],
+                       help='DATA, BASIC_[CNN|MLP], or VGG16')
     group.add_argument('-d', '--dataset', required=True, type=str,
-                       help='the dataset to be used')
+                       help='the dataset path, must have the folder structure: training/train, training/val and test,'
+                            'in each of this folders, one folder per class (see dataset_test)')
     # OPTIONAL Arguments
     group.add_argument('-o', '--output_model', required=False, type=str, default=None,
-                       help='Name of model to output and store')
+                       help='Name of model to store')
     group.add_argument('-e', '--epochs', required=False, type=int, default=10,
                        help='number of epochs')
     group.add_argument('-b', '--batch_size', required=False, type=int, default=32)
@@ -108,6 +137,12 @@ def parse_args():
     group.add_argument('-w', '--weights', required=False, type=str, default=None,
                        help="If you do not want random initialization of the model weights "
                             "(ex. 'imagenet' or path to weights to be loaded)")
+    group.add_argument('--mode', required=False, type=str, default='train-val', choices=['train-val', 'train-test',
+                                                                                         'test'],
+                       help="Choose which mode run between 'train-val' (default) or 'train-test'."
+                            "The 'train-val' mode will run a phase of training and validation on the training and "
+                            "validation set, while the 'train-test' mode will run a phase of training on the "
+                            "training+validation sets and then test on the test set.")
     # FLAGS
     group.add_argument('--exclude_top', dest='include_top', action='store_false',
                        help='Exclute the fully-connected layer at the top pf the network (default INCLUDE)')
@@ -117,10 +152,6 @@ def parse_args():
 
 
 def _check_args(arguments):
-    if arguments.model != "BASIC" \
-            and arguments.model != "VGG16":
-        print('Invalid model choice, exiting...')
-        exit()
     if re.match(r"^\d{2,4}x([13])$", arguments.image_size):
         img_size = arguments.image_size.split("x")[0]
         channels = arguments.image_size.split("x")[1]
@@ -132,10 +163,11 @@ def _check_args(arguments):
     if not os.path.isdir(MAIN_PATH + arguments.dataset):
         print('Cannot find dataset in {}, exiting...'.format(arguments.dataset))
         exit()
-    if "_PREPROCESSED" not in MAIN_PATH + arguments.dataset:
-        # TODO implement checks on the dataset folder structure
-        # Naive approach: just check foldername if contain _PREPROCESSED
-        print("Dataset '{}' is not marked as '_PREPROCESSED'...".format(arguments.dataset))
+    # Check Dataset struct: should be in folder tree training/[train|val] e test
+    if not os.path.isdir(MAIN_PATH + arguments.dataset + "/test") or \
+            not os.path.isdir(MAIN_PATH + arguments.dataset + "/training/val") or \
+            not os.path.isdir(MAIN_PATH + arguments.dataset + "/training/train"):
+        print(f"Dataset '{arguments.dataset}' should contain folders 'test, training/train and training/val'...")
         exit()
 
 
@@ -143,8 +175,12 @@ def _model_selection(arguments, nclasses):
     model = None
     print("INITIALIZING MODEL")
 
-    if arguments.model == "BASIC":
-        model_class = BASIC(nclasses, arguments.image_size, arguments.channels)
+    if arguments.model == "BASIC_CNN":
+        model_class = BasicCNN(nclasses, arguments.image_size, arguments.channels)
+    elif arguments.model == "BASIC_MLP":
+        print("TODO: integrate MLP model! Code available for educational purpose only, exiting...")
+        exit()
+        # model_class = BasicMLP(nclasses, <VECTOR_SIZE>)
     elif arguments.model == "VGG16":
         # NB. Setting include_top=True and thus accepting the entire struct, the input Shape MUST be 224x224x3
         # and in any case, channels has to be 3
@@ -243,4 +279,3 @@ if __name__ == '__main__':
     end = time.perf_counter()
     print()
     print_log("EX. TIME: {} ".format(str(datetime.timedelta(seconds=end-start))), print_on_screen=True)
-
